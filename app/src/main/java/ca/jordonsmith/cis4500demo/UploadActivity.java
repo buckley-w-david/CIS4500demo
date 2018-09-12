fd1879448f3c16b0798936ae98be89d9ac2c8219
David Buckley <buckley.w.david@gmail.com>
2018-09-12 02:12:34 -0400

    Complete UploadActivity Entrypoint section

--- app/src/main/java/ca/jordonsmith/cis4500demo/UploadActivity.java
+++ app/src/main/java/ca/jordonsmith/cis4500demo/UploadActivity.java
@@ -20,6 +20,35 @@ public class UploadActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload);
 
+        /*
+         *  Code example from Android Documentation available:
+         *  https://developer.android.com/training/sharing/receive
+         *
+         *  Responsible for retrieving additional text from the intent
+         *  when this activity is used as an entry point via Android's
+         *  "sharing" option.
+         * */
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        String type = intent.getType();
+
+        if (Intent.ACTION_SEND.equals(action) && type != null) {
+            if ("text/plain".equals(type)) {
+                handleSendText(intent);
+            }
+        }
+    }
+
+    /*
+     *  Ran when UploadActivity is used as an entry point into the application
+     *  places supplied text into the text field in the UI
+     * */
+    private void handleSendText(Intent intent) {
+        String sharedText = intent.getStringExtra(Intent.EXTRA_TEXT);
+        if (sharedText != null) {
+            EditText textBox = findViewById(R.id.uploadText);
+            textBox.setText(sharedText);
+        }
     }
 
     // Ran when the app comes into the foreground after being hidden
@@ -46,7 +75,7 @@ public class UploadActivity extends AppCompatActivity {
      * to a remote server
      * */
     public void onUploadClicked(View view) {
-        EditText textBox = (EditText) findViewById(R.id.uploadText);
+        EditText textBox = findViewById(R.id.uploadText);
         String content = textBox.getText().toString();
         UploadService.startActionUpload(this, content);
     }
