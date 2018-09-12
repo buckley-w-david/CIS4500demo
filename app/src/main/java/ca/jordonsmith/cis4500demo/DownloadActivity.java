522bcfcc928971d365397f8134bcece50d6e3f59
David Buckley <buckley.w.david@gmail.com>
2018-09-12 02:23:42 -0400

    Complete DownloadService section

--- app/src/main/java/ca/jordonsmith/cis4500demo/DownloadActivity.java
+++ app/src/main/java/ca/jordonsmith/cis4500demo/DownloadActivity.java
@@ -4,7 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+
 public class DownloadActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,5 +32,6 @@ public class DownloadActivity extends AppCompatActivity {
     * Starts the download service
     * */
     public void onDownloadClicked(View view) {
+        DownloadService.startActionDownload(this);
     }
 }
